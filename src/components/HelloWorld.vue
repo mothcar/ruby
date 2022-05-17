d6f092d65158084163949b14b8b8a046db7909fb
Paul Hwang <mothcar@gmail.com>
2022-05-17 20:16:30 +0900

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import logo from '../assets/logo.svg'
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    
+  }),
+
+  
+
+  mounted() {
+    // console.log('I am mounted...')
+    this.hello()
+  },
+
+  created() {
+    console.log('I am created...')
+    console.log('this : ', this)
+    this.dady()
+  },
+
+
+
+
+
+  methods: {
+    hello() {
+      console.log('I am hello...')
+    },
+
+    dady() {
+      console.log('I am your father...')
+    }
+    
+  },
+}
+</script>
